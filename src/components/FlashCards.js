b4458d06f17c570dba728e8242efba6ba44bbd4c
medinar <rommel.d.medina@gmail.com>
2021-11-21 18:39:12 -0800

    add sample definitions

--- src/components/FlashCards.js
+++ src/components/FlashCards.js
@@ -1,5 +1,6 @@
 import "tailwindcss/tailwind.css"
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import { definitions } from "../data";
 
 import React from "react";
 
@@ -7,24 +8,37 @@ export default function FlashCards() {
   return (
     <section id="flash-cards">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1>Flash Cards</h1>
-            <Flippy
-                flipOnHover={false} // default false
-                flipOnClick={true} // default false
-                flipDirection="horizontal" // horizontal or vertical
-                // ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
-                // if you pass isFlipped prop component will be controlled component.
-                // and other props, which will go to div
-                style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
-            >
-                <FrontSide className="bg-white">
-                    Question
-                </FrontSide>
-                <BackSide className="bg-gray-900 text-green-900">
-                    Answer
-                </BackSide>
-            </Flippy>
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0">
+            <h1 className="text-2xl my-5">Flash Cards</h1>
+            {definitions.map((def) => (
+                <Flippy
+                    flipOnHover={false} // default false
+                    flipOnClick={true} // default false
+                    flipDirection="vertical" // horizontal or vertical
+                    // ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+                    // if you pass isFlipped prop component will be controlled component.
+                    // and other props, which will go to div
+                    // style={{ width: '500px', height: '150px' }} /// these are optional style, it is not necessary
+                    className="my-3 w-6/12 h-72"
+                >
+                    <FrontSide className="bg-white text-4xl">
+                        {def.term}                        
+                    </FrontSide>
+                    <BackSide className="text-gray-50 text-lg" style={{ backgroundColor: '#047857'}}>
+                        <p className="mb-3">{def.definition}</p>
+                        <p>
+                            <a
+                                className="App-link"
+                                href={def.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {def.term}
+                            </a>
+                        </p>
+                    </BackSide>
+                </Flippy>
+            ))}
         </div>
       </div>
     </section>
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const definitions = [
+  {
+    term: "Java",
+    definition: "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
+    link: "https://en.wikipedia.org/wiki/Java_(programming_language)"
+  },
+  {
+    term: "Spring Framework",
+    definition: "Spring Framework is one of the most popular Java-based application frameworks. The Spring Framework was developed by *Rod Johnson* back in *2003*. The Spring Framework is an open-source framework that can be used to develop Java applications with ease and at a rapid pace. It is a lightweight framework that also provides well-defined infrastructure support for developing applications in Java. In other words, you can say that Spring handles the infrastructure so that you can focus more on developing your application.",
+    link: "https://en.wikipedia.org/wiki/Spring_Framework"
+  },
+  {
+    term: "Spring Boot",
+    definition: "Spring Boot is basically an extension of the Spring framework, which eliminates the boilerplate configurations required for setting up a Spring application.",
+    link: "https://www.tutorialspoint.com/spring_boot/spring_boot_introduction.htm#:~:text=Spring%20Boot%20is%20an%20open,you%20with%20its%20basic%20concepts."
+  },
+  {
+    term: "Maven",
+    definition: "Maven is a build automation tool used primarily for Java projects. Maven can also be used to build and manage projects written in C#, Ruby, Scala, and other languages. The Maven project is hosted by the Apache Software Foundation, where it was formerly part of the Jakarta Project.",
+    link: "https://maven.apache.org/"
+  },
+  {
+    term: "TypeScript",
+    definition: "TypeScript is JavaScript with syntax for types. TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.",
+    link: "https://www.typescriptlang.org/"
+  },
+  {
+    term: "MVC Architecture",
+    definition: "A Spring MVC is a Java framework which is used to build web applications. It follows the Model-View-Controller design pattern. It implements all the basic features of a core spring framework like Inversion of Control, Dependency Injection.",
+    link: "https://towardsdatascience.com/everything-you-need-to-know-about-mvc-architecture-3c827930b4c1"
+  },
+  {
+    term: "Spring MVC",
+    definition: "MVC is an architectural pattern consisting of three parts: Model, View, Controller. Model: Handles data logic. View: It displays the information from the model to the user. Controller: It controls the data flow into a model object and updates the view whenever data changes.",
+    link: "https://www.javatpoint.com/spring-mvc-tutorial#:~:text=A%20Spring%20MVC%20is%20a,Inversion%20of%20Control%2C%20Dependency%20Injection."
+  }
+
+];
