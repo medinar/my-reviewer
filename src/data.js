a642f8779b0fcd5307c1064f83d58739ee8fd759
medinar <rommel.d.medina@gmail.com>
2021-11-22 12:07:32 -0800

    add more cards

--- src/data.js
+++ src/data.js
@@ -33,6 +33,76 @@ export const definitions = [
     term: "Spring MVC",
     definition: "MVC is an architectural pattern consisting of three parts: Model, View, Controller. Model: Handles data logic. View: It displays the information from the model to the user. Controller: It controls the data flow into a model object and updates the view whenever data changes.",
     link: "https://www.javatpoint.com/spring-mvc-tutorial#:~:text=A%20Spring%20MVC%20is%20a,Inversion%20of%20Control%2C%20Dependency%20Injection."
+  },
+  {
+    term: "Hibernate",
+    definition: "Hibernate ORM is an object–relational mapping tool for the Java programming language. It provides a framework for mapping an object-oriented domain model to a relational database.",
+    link: "https://hibernate.org/"
+  },
+  {
+    term: "REST",
+    definition: "A REST API (also known as RESTful API) is an **application programming interface** (API or web API) that conforms to the constraints of REST architectural style and allows for interaction with RESTful web services. REST stands for representational state transfer and was created by computer scientist Roy Fielding.",
+    link: "https://www.redhat.com/en/topics/api/what-is-a-rest-api"
+  },
+  {
+    term: "SOAP",
+    definition: "Simple Object Access Protocol is the type of communication between  multiple application over the internet or the world wide web. It is associated with another file called the Web Service Description Language (WSDL). It provides the definition of haw the web service works. SOAP is a messaging protocol specification for exchanging structured information in the implementation of web services in computer networks.",
+    link: "https://en.wikipedia.org/wiki/SOAP"
+  },
+  {
+    term: "Javascript",
+    definition: "JavaScript is a scripting or programming language that allows you to implement complex features on web pages and run it on the browser.",
+    link: "https://en.wikipedia.org/wiki/JavaScript"
+  },
+  {
+    term: "Jasper Reports",
+    definition: "**JasperReports** is an [open source](https://en.wikipedia.org/wiki/Open-source_software) [Java](https://en.wikipedia.org/wiki/Java_(programming_language)) [reporting](https://en.wikipedia.org/wiki/Report) tool that can write to a variety of targets, such as: screen, a printer, into [PDF](https://en.wikipedia.org/wiki/Portable_Document_Format), [HTML](https://en.wikipedia.org/wiki/HTML), [Microsoft Excel](https://en.wikipedia.org/wiki/Microsoft_Excel), [RTF](https://en.wikipedia.org/wiki/Rich_Text_Format), [ODT](https://en.wikipedia.org/wiki/OpenDocument), [comma-separated values](https://en.wikipedia.org/wiki/Comma-separated_values) (CSV) or [XML](https://en.wikipedia.org/wiki/XML) files. It can be used in Java-enabled applications, including [Java EE](https://en.wikipedia.org/wiki/Java_EE) or [web applications](https://en.wikipedia.org/wiki/Web_application), to generate dynamic content. It reads its instructions from an XML or `.jasper` file.",
+    link: "https://community.jaspersoft.com/"
+  },
+  {
+    term: "Velocity",
+    definition: "The VelocityStruts sub-project integrates Velocity with the [Apache Struts](http://struts.apache.org/) web application framework and enables the use of Velocity templates interchangeably with JSP pages for the view layer. Various other [web application frameworks](http://wiki.apache.org/velocity/PoweredByVelocity) offer built-in support for Velocity templates. This project provides the minimal glue necessary to give Struts developers an alternative to JSP.",
+    link: "https://velocity.apache.org/tools/2.0/struts.html"
+  },
+  {
+    term: "Struts",
+    definition: "Apache Struts is a free, open-source, MVC framework for creating elegant, modern Java web applications. It favors convention over configuration, is extensible using a plugin architecture, and ships with plugins to support REST, AJAX and JSON.",
+    link: "https://struts.apache.org/"
+  },
+  {
+    term: "Scrum",
+    definition: "Scrum is a framework that helps teams work together. Much like a rugby team (where it gets its name) training for the big game, scrum encourages teams to learn through experiences, self-organize while working on a problem, and reflect on their wins and losses to continuously improve. While the scrum I’m talking about is most frequently used by software development teams, its principles and lessons can be applied to all kinds of teamwork. This is one of the reasons scrum is so popular. Often thought of as an agile project management framework, scrum describes a set of meetings, tools, and roles that work in concert to help teams structure and manage their work.",
+    link: "https://www.atlassian.com/agile/scrum"
+  },
+  {
+    term: "Git",
+    definition: "By far, the most widely used modern version control system in the world today is Git. Git is a mature, actively maintained open source project originally developed in 2005 by Linus Torvalds, the famous creator of the Linux operating system kernel. A staggering number of software projects rely on Git for version control, including commercial projects as well as open source. Developers who have worked with Git are well represented in the pool of available software development talent and it works well on a wide range of operating systems and IDEs (Integrated Development Environments).",
+    link: "https://www.atlassian.com/git/tutorials/what-is-git"
+  },
+  {
+    term: "Selenium",
+    definition: "Selenium is an open-source umbrella project for a range of tools and libraries aimed at supporting web browser automation. Selenium provides a playback tool for authoring functional tests without the need to learn a test scripting language.",
+    link: "https://www.selenium.dev"
+  },
+  {
+    term: "Flutter",
+    definition: "Flutter is an open-source UI software development kit created by Google. It is used to develop cross platform applications for Android, iOS, Linux, Mac, Windows, Google Fuchsia, and the web from a single codebase. First described in 2015, Flutter was released in May 2017.",
+    link: "https://flutter.dev/?gclid=Cj0KCQiA-eeMBhCpARIsAAZfxZD-q9A1tmNPlLCKVhSKXDr7V69_p3EQY2Gf1bQ6sgN86F1YkbkElgMaArtdEALw_wcB&gclsrc=aw.ds"
+  },
+  {
+    term: "Angular",
+    definition: "Angular is a TypeScript-based free and open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.",
+    link: "https://angular.io/"
+  },
+  {
+    term: "HL7",
+    definition: "Founded in 1987, Health Level Seven International (HL7) is a not-for-profit, ANSI-accredited standards developing organization dedicated to providing a comprehensive framework and related standards for the exchange, integration, sharing and retrieval of electronic health information that supports clinical practice and the management, delivery and evaluation of health services.",
+    link: "http://www.hl7.org/"
+  },
+  {
+    term: "FIHR",
+    definition: "FHIR (Fast Healthcare Interoperability Resources) Specification, which is a standard for exchanging healthcare information electronically",
+    link: "http://hl7.org/fhir/STU3/overview.html"
   }
 
 ];
